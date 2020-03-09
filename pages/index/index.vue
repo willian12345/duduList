76c0ab6255c9e1d0ee7be234100f3495c6bd90bf
sheldon <willian12345@126.com>
2020-03-09 15:23:19 +0800

    Update index.vue
    多显示前一页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,7 +8,10 @@
 			:scroll-with-animation="true"
 			@scroll="scroll">
 			<view class="list">
-				<view class="list-item" v-for="(item, i) in list" :key="i" :id="'item'+item.userId">
+				<view 
+					class="list-item"
+					v-for="(item, i) in list" :key="i" 
+					:id="'item'+item.userId">
 					<view class="flex-row list-row fake-item" v-if="!item.visible">
 						<view class="avatar">{{i}}</view>
 						<view class="col">
@@ -16,28 +19,18 @@
 							</view>
 						</view>
 					</view>
-					<view class="flex-row list-row" v-else>
+					<view 
+						v-else
+						class="flex-row list-row">
 						<image :src="item.avatar" class="avatar"></image>
 						<view class="col">
 							<view class="flex-row texts">
 								<view class="nickname">{{item.nickName}}</view>
-								<view class="sign-info">
-									<text v-if="item.friendBy">  “{{item.friendBy}}”的好友</text>
-									<view v-else-if="item.sourceId">来自“<open-data type="groupName" :open-gid="item.sourceId"></open-data>”群聊</view>
-									<text v-else>微信好友</text>
-								</view>
+								<view class="sign-info">微信好友</view>
 							</view>
 							<view class="mood">{{item.remarks}}</view>
 						</view>
-						<view class="status" :class="[item.statusIcon]" v-if="type == 1"></view>
-						<view class="status remind not-remind" v-else-if="item.friendBy">
-							<text>无法提醒</text>
-						</view>
-						<view class="status remind" :class="{'reminded': item.remind == 0}" v-else>
-							<text v-if="item.remind == 0">已提醒</text>
-							<button v-else open-type="share" class="status remind" @click.stop="remind(item)">提醒TA</button>
-						</view>
-						<view class="arrow collapse-btn" :class="{'arrow-up': item.show}"></view>
+						<view class="status remind"><text>{{i}}</text></view>
 					</view>
 				</view>
 			</view>
@@ -46,30 +39,33 @@
 </template>
 <script>
 	import Vue from 'vue'
+	// 共291条假数据
 	let listData = require('../data.json')
-	
 	export default{
 		name: 'List',
 		data() {
 			return {
-				listHeight: 400,
+				listHeight: 500,
+				itemHeight: 74,
 				scrollTop: 0,
-				index: 0,
+				pageOffset: 0,
 				pageSize: 10,
+				pageTotal: Math.ceil(listData.data.length / 10),
 				list: [],
-				old: {
-					scrollTop: 0
-				}
 			}
 		},
 		methods: {
+			// 模拟加载显示下一页
 			showNext(){
 				let arr = listData.data
-				if(this.index * this.pageSize >= arr.length){
+				
+				if(this.pageOffset * this.pageSize >= arr.length){
+					// 分页加载完了
 					return
 				}
-				this.index++
-				arr = arr.slice(this.index * this.pageSize - this.pageSize, this.index * this.pageSize)
+				
+				this.pageOffset++
+				arr = arr.slice(this.pageOffset * this.pageSize - this.pageSize, this.pageOffset * this.pageSize)
 				arr.map( v => {
 					this.$set(v, 'visible', true)
 				})
@@ -78,20 +74,22 @@
 			lower(){
 				this.showNext()
 			},
+			// 当前页的前页的前页隐藏
 			hidePrev(page){
-				if(page - 1 > 0){
-					for(let i=0; i< (page-1) * this.pageSize; i++){
+				if(page - 2 > 0){
+					for(let i=0; i< (page - 2) * this.pageSize; i++){
 						this.list[i].visible = false
 					}
 				}
 			},
+			// 当前页的下而的下页隐藏
 			hideNext(page){
-				if(page + 1 <= this.index){
-					let last = ((this.index - page) + page) * this.pageSize
+				if(page + 1 <= this.pageOffset){
+					let last = ((this.pageOffset - page) + page) * this.pageSize
 					if(last > this.list.length){
 						last = this.list.length
 					}
-					for(let i=(page + 1) * this.pageSize; i< last; i++){
+					for(let i= (page + 1) * this.pageSize; i< last; i++){
 						this.list[i].visible = false
 					}	
 				}
@@ -99,11 +97,10 @@
 			scroll(e){
 				let scrollHeight = e.detail.scrollHeight
 				let scrollTop = e.detail.scrollTop
-				// 通过滚动条滚动到的位置 / 每页占用的高度计算出当前是第几页
-				let page = Math.round(scrollTop / (this.pageSize * 74))
-				console.log(page)
+				// 通过滚动条滚动到的位置 / 每页占用的高度计算出当前处于第几页
+				let page = Math.ceil(scrollTop / (this.pageSize * this.itemHeight))
 				
-				// 隐藏掉page之前的所有条目
+				// 滚动列表前一部分
 				this.hidePrev(page)
 				
 				// 只显示当前page对应的条目
@@ -140,7 +137,6 @@
 		padding: 24upx 0;
 		height: 148upx;
 		overflow: hidden;
-		background-color: red;
 		border-bottom: 1px solid green;
 		.avatar{
 			margin-right: 16upx;
@@ -197,58 +193,6 @@
 			background: none;
 		}
 	}
-	.icon-share{
-		display: inline-flex;
-		align-items: center;
-	}
-	.icon-share{
-		margin-right: 16upx;
-		width: 36upx;
-		height: 36upx;
-		background-image: url(../../static/image/icon-share-white.png);
-		background-size: 100%;
-	}
-	.no-data{
-		padding-top: 64upx;
-		text-align: center;
-		font-size: 32upx;
-		color: #A6A6A6;
-	}
-	.bottom-view{
-		padding: 48upx 0 88upx;
-	}
-	.share-btn{
-		margin: 0 auto 0;
-		width: 480upx;
-		height: 80upx;
-		line-height: 80upx;
-		font-size: 32upx;
-		color: white;
-		background: rgba(51,204,136,1);
-		box-shadow: 0 4upx 16upx 0 rgba(36,169,110,0.5);
-		border-radius: 40upx;
-	}
-	.health{
-		background-image: url(~@/static/image/components/poster/health.png);
-	}
-	.health-normal{
-		background-image: url(~@/static/image/components/poster/health-normal.png);
-	}
-	.health-bad{
-		background-image: url(~@/static/image/components/poster/health-bad.png);
-	}
-	.health-unsign{
-		background-image: url(~@/static/image/components/poster/health-unsign.png);
-	}
-	.health-min{
-		background-image: url(~@/static/image/health.png);
-	}
-	.health-normal-min{
-		background-image: url(~@/static/image/health-normal.png);
-	}
-	.health-bad-min{
-		background-image: url(~@/static/image/health-bad.png);
-	}
 	
 	.fake-item{
 		.avatar{
@@ -259,5 +203,8 @@
 			height: 40upx;
 			background-color: #f2f2f2;
 		}
+	}
+	.page-divider{
+		background-color: green;
 	}
 </style>
